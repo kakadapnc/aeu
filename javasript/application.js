25303a740d979b83170a1728ddcef69b036ed827
Kakada Neang <kakada@web-essentials.asia>
2016-01-09 09:01:39 +0700

    [TASK] Order structure
    refs order

--- javasript/application.js
+++ javasript/application.js
@@ -1,30 +1,19 @@
 /**
-** Custom JavaScript
-**/
-
-var IncludeContent = function(data) {
-	var xmlhttp = new XMLHttpRequest();
-	xmlhttp.open("GET", data, false);
-	xmlhttp.send();
-	return xmlhttp.response;
-}
-
-var menu = document.getElementById("header").innerHTML = IncludeContent("templates/partials/Menu.html");
-var slideShow = document.getElementById("slide").innerHTML = IncludeContent("templates/partials/SlideShow.html");
-//var basseFood = document.getElementById("baseFood").innerHTML = IncludeContent("templates/partials/baseFood.html");
-//var ourChef = document.getElementById("ourChef").innerHTML = IncludeContent("templates/partials/ourChef.html");
-//var footer = document.getElementById("footer").innerHTML = IncludeContent("templates/partials/Footer.html");
-
-
-/**
- * slide
- **/
+ * delay time interval of carousel
+ */
+$('.carousel').carousel({
+	interval: 1000 * 10
+});
 
 var changePhoto=0;
 var opacitys=0;
 var outAndIn = true;
 var photo = ["b.jpg", "a.jpg","b.jpg","a.jpg","b.jpg","a.jpg","b.jpg","a.jpg"];
-function runners() { //this fx will make your photo come-in and go-out slowly
+
+/**
+ * Start slide-show
+ */
+function runners() {
 	for (var i =0; i<photo.length; i++) {
 	}
 	var runner=setInterval(function() {
@@ -36,83 +25,116 @@ function runners() { //this fx will make your photo come-in and go-out slowly
 		}else{
 			opacitys-=0.02;
 			var opacity = da.style.opacity=opacitys;
-			if(opacitys< -0.3){
+			if(opacity< -0.3){
 				outAndIn=true;
 				changePhoto++;
 				if(changePhoto>2){changePhoto=0}
 			}
 		}
-	}, 25);
+	}, 35);
 }
-function changePhotos () {// this fx will change the photo on slide next and pre
+
+/**
+ *  Change image in circle
+ **/
+function changePhotos () {
 	if(changePhoto>=photo.length)
 		changePhoto= 0;
 	else if(changePhoto<0)
 		changePhoto= photo.length-1;
 	da.src=photo[changePhoto];
 }
-function next () {// this fx will change photo to by + 1 to variable changePhoto and call changePhotos
+
+/**
+ * Change image to next
+ */
+function next () {
 	changePhoto++;
 	changePhotos ();
 }
-// this fx will change photo to by - 1 to variable changePhoto and call changePhotos
+
+/**
+ * Change image to pre
+ */
 function pre () {
 	changePhoto--;
 	changePhotos ();
 }
-runners();
-/***************************/
 
-var menuLisst = function() {
-	event.preventDefault();
-var menuList = document.getElementById("menulist").innerHTML = IncludeContent("templates/partials/menuList.html");
+/**
+ * Include file
+ * @param data
+ * @returns {Object}
+ * @constructor
+ */
+var IncludeContent = function(data) {
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.open("GET", data, false);
+	xmlhttp.send();
+	return xmlhttp.response;
 }
 
-//var menuList = document.getElementById("menulist").innerHTML = IncludeContent("templates/partials/menuList.html");
-//Menu List
+/**
+ * Load partails into index
+ */
+var loadPartails = function() {
+	document.getElementById("slide").innerHTML = IncludeContent("partials/SlideShow.html");
+	document.getElementById("ourChef").innerHTML = IncludeContent("partials/ourChef.html");
+	document.getElementById("baseFood").innerHTML = IncludeContent("partials/baseFood.html");
+	runners();
+}
+var slide = function() {
+	document.getElementById("slide").innerHTML = IncludeContent("partials/SlideShow.html");
+	runners();
+}
+
+var foodDetail = function() {
+	document.getElementById("slide").innerHTML = IncludeContent("partials/SlideShow.html");
+	document.getElementById("foodDetail").innerHTML = IncludeContent("partials/foodDetail.html");
+	document.getElementById("footer").innerHTML = IncludeContent("partials/footer.html");
+	runners();
+}
+
+var mainRoom = function() {
+	document.getElementById("mainRoom").innerHTML = IncludeContent("partials/mainRoom.html");
+}
+
+var vipRoom = function() {
+	document.getElementById("vipRoom").innerHTML = IncludeContent("partials/vipRoom.html");
+}
+
+var order = function() {
+	document.getElementById("order").innerHTML = IncludeContent("partials/order.html");
+}
+
+document.getElementById("header").innerHTML = IncludeContent("partials/Menu.html");
+document.getElementById("footer").innerHTML = IncludeContent("partials/footer.html");
+
+
+
+
 
 var menu = [
 	"Fried Rice", "Fried Egg", "Fried Fish", "Fried Chicken", "Fried Egg With Salad",
-	"Fried Rice with Egg", "Fried Rice with Sea Food", "Fried Rice with Bef"
+	"Fried Rice with Egg", "Fried Rice with Sea Food", "Fried Rice with Bef", "Fish Fried",
+	"Ham Fried", "Pig Fried", "Soup Fish", "Soup Bef", "Soup Chicken", "Kor Kor Soup"
 ];
-
-function menuList(){
-	event.preventDefault();
-	var menuList = document.getElementById("menulist").innerHTML = IncludeContent("templates/partials/menuList.html");
-	var list = "";
-	for (var i=0; i<=menu.length; i++) {
-		list += '<li onclick="menulists();"><a href="#friedRice"><span>'+menu[i]+'</span>'
-				+'...................................................................'
-				+'...................................................................'
-				+'...................................................................'
-				+'<span class="pull-right">$12</span></a></li>';
-		//alert(list);
-	}
-	document.getElementById("test").innerHTML= "<ol>"+list+"</ol>";
-}
-//menulists();
-//var menu1 = {
-//	"Fried Rice": 12,
-//	"Fried Egg": 4,
-//}
-//
-//console.log(menu1['Fried Rice']);
-//console.log(menu1);
-
-//var menuList = document.getElementById("menulist").innerHTML = IncludeContent("templates/partials/menuList.html");
+var price = [12, 7, 13, 12, 10, 15, 7, 4, 2, 6, 3, 18, 12, 18, 25]
 
 /**
- * llllllllllllllllllllllllllll
+ * List menu
  */
+function foodMenu(){
+	var list = "";
+	for (var i=0; i<menu.length; i++) {
+		list += '<li> <a href="FoodDetail.html#'+menu[i]+'"><span class="bg-list">'+ menu[i]+'</span><span class="pull-right bg-list">$'+price[i]+'</span></a> <p class="lineMenu"></p> </li>'
+	}
+	document.getElementById("FoodMenu").innerHTML= "<ol class='menuList'>"+list+"</ol>";
+	document.getElementById("footer").innerHTML = IncludeContent("partials/footer.html");
+	slide();
+	runners();
 
-
-
-
-
-
-
-
-
+}
 
 
 
