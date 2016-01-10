667b0924d86faaa49edc0591406dfc9645e7b234
Kakada Neang <kakada@web-essentials.asia>
2016-01-10 19:23:58 +0700

    [Refactor]

--- javasript/application.js
+++ javasript/application.js
@@ -1,9 +1,6 @@
 /**
  * delay time interval of carousel
  */
-$('.carousel').carousel({
-	interval: 1000 * 10
-});
 
 var changePhoto=0;
 var opacitys=0;
@@ -74,52 +71,38 @@ var IncludeContent = function(data) {
 	return xmlhttp.response;
 }
 
-/**
- * Load partails into index
- */
-var loadPartails = function() {
-	document.getElementById("slide").innerHTML = IncludeContent("partials/SlideShow.html");
-	document.getElementById("ourChef").innerHTML = IncludeContent("partials/ourChef.html");
-	document.getElementById("baseFood").innerHTML = IncludeContent("partials/baseFood.html");
-	runners();
-}
 var slide = function() {
 	document.getElementById("slide").innerHTML = IncludeContent("partials/SlideShow.html");
 	runners();
 }
 
-var foodDetail = function() {
-	document.getElementById("slide").innerHTML = IncludeContent("partials/SlideShow.html");
-	document.getElementById("foodDetail").innerHTML = IncludeContent("partials/foodDetail.html");
-	document.getElementById("footer").innerHTML = IncludeContent("partials/footer.html");
-	runners();
-}
-
-var mainRoom = function() {
-	document.getElementById("mainRoom").innerHTML = IncludeContent("partials/mainRoom.html");
-}
-
-var vipRoom = function() {
-	document.getElementById("vipRoom").innerHTML = IncludeContent("partials/vipRoom.html");
-}
-
-var order = function() {
-	document.getElementById("order").innerHTML = IncludeContent("partials/order.html");
+function loadPage(page) {
+	event.preventDefault();
+	if (page =='orderPage') {
+		document.getElementById("dynamicContent").innerHTML = IncludeContent("partials/order.html");
+	}else if(page == "menuListPage") {
+		foodMenu();
+	}else if(page == "menuDetailPage") {
+		document.getElementById("dynamicContent").innerHTML = IncludeContent("partials/foodDetail.html");
+	}else if(page == "mainRoomPage") {
+		document.getElementById("dynamicContent").innerHTML = IncludeContent("partials/mainRoom.html");
+	}else if(page == "vipRoomPage") {
+		document.getElementById("dynamicContent").innerHTML = IncludeContent("partials/vipRoom.html");
+	} else {
+		document.getElementById("dynamicContent").innerHTML = IncludeContent("partials/SlideShow.html") + IncludeContent("partials/ourChef.html") + IncludeContent("partials/baseFood.html");
+	}
 }
 
 document.getElementById("header").innerHTML = IncludeContent("partials/Menu.html");
 document.getElementById("footer").innerHTML = IncludeContent("partials/footer.html");
 
-
-
-
-
 var menu = [
 	"Fried Rice", "Fried Egg", "Fried Fish", "Fried Chicken", "Fried Egg With Salad",
 	"Fried Rice with Egg", "Fried Rice with Sea Food", "Fried Rice with Bef", "Fish Fried",
-	"Ham Fried", "Pig Fried", "Soup Fish", "Soup Bef", "Soup Chicken", "Kor Kor Soup", "Arathna","test"
+	"Ham Fried", "Pig Fried", "Soup Fish", "Soup Bef", "Soup Chicken", "Kor Kor Soup", "Arathna",
+	"test", "Salad & bef"
 ];
-var price = [12, 7, 13, 12, 10, 15, 7, 4, 2, 6, 3, 18, 12, 18, 25, 24,100]
+var price = [12, 7, 13, 12, 10, 15, 7, 4, 2, 6, 3, 18, 12, 18, 25, 24,100, 23]
 
 /**
  * List menu
@@ -129,25 +112,10 @@ function foodMenu(){
 	for (var i=0; i<menu.length; i++) {
 		list += '<li> <a href="FoodDetail.html#'+menu[i]+'"><span class="bg-list">'+ menu[i]+'</span><span class="pull-right bg-list">$'+price[i]+'</span></a> <p class="lineMenu"></p> </li>'
 	}
-	document.getElementById("FoodMenu").innerHTML= "<ol class='menuList'>"+list+"</ol>";
-	document.getElementById("footer").innerHTML = IncludeContent("partials/footer.html");
+	document.getElementById("dynamicContent").innerHTML= "<div class='col-sm-10 col-sm-offset-1'><ol class='menuList'>"+list+"</ol></div>";
 	slide();
 	runners();
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
